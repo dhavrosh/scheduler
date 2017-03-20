3229abb2d47c8fd250d104afe4128fc46879819b
DESKTOP-3MBDHF9\dhavr <dhavrosh@gmail.com>
2017-03-20 23:40:29 +0200

    finish styling of preview-class component

--- src/modules/class/PreviewClassView.js
+++ src/modules/class/PreviewClassView.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
 import {
     StyleSheet,
     TouchableOpacity,
@@ -7,45 +7,90 @@ import {
     TextInput,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { pushRoute, setRightComponentAction } from '../navigation/NavigationState';
-
+import {pushRoute, setRightComponentAction} from '../navigation/NavigationState';
 
 const PreviewClassView = React.createClass({
     redirectToEditClass(Class){
         this.props.dispatch(pushRoute({
             key: 'EditClass',
             title: `Edit Class`,
-            data: { Class, isUpdate: Class && true || false },
+            data: {Class, isUpdate: Class && true || false},
             showRightComponent: 'true',
             iconName: 'save',
         }));
     },
     componentDidMount() {
-        const { data: { Class } } = this.props;
+        const {data: {Class}} = this.props;
 
         setTimeout(() => this.props.dispatch(setRightComponentAction(
             () => this.redirectToEditClass(Class)
-        )), 300);
+        )), 500);
     },
     render() {
-        const { data: { Class } } = this.props;
+        const {data: {Class}} = this.props;
 
         return (
             <View style={ styles.container }>
-                <Text>{ Class.title.value }</Text>
+                <View style={styles.mainSection}>
+                    <View style={styles.icons}>
+                        <Icon name="calendar" size={30}/>
+                        <Icon name="map-marker" size={30}/>
+                        <Icon name="user" size={30}/>
+                    </View>
+                    <View style={styles.texts}>
+                        <Text >{ Class.title.value }</Text>
+                        <Text >{ Class.title.value }</Text>
+                        <Text >{ Class.title.value }</Text>
+                    </View>
+                </View>
+                <View style={styles.secondarySection}>
+                    <Text style={styles.descriptionTitle}>Description</Text>
+                    <Text style={styles.descriptionText}>{ Class.description.value }</Text>
+                </View>
             </View>
-        );
+    );
     }
-});
+    });
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        marginLeft: 10,
-        marginRight: 10
-    }
-});
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            paddingHorizontal: 20,
+        },
+        mainSection: {
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            height: 180,
+            paddingVertical: 20,
+            paddingHorizontal: 20,
+            borderBottomWidth: 0.5,
+            borderBottomColor: 'gray'
+        },
+        secondarySection: {
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            paddingVertical: 15,
+            paddingHorizontal: 20,
+        },
+        icons: {
+            flexDirection: 'column',
+            alignItems : 'center',
+            justifyContent: 'space-between',
+        },
+        texts: {
+            marginLeft: 40,
+            paddingVertical: 5,
+            flexDirection: 'column',
+            alignItems : 'center',
+            justifyContent: 'space-between'
+        },
+        descriptionTitle: {
+            fontSize: 16
+        },
+        descriptionText: {
+            marginTop: 10,
+            color: 'gray'
+        }
+    });
 
-export default PreviewClassView;
+    export default PreviewClassView;
